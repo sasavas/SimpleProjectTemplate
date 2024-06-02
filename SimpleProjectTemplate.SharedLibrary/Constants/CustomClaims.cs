80cfe772b2d5fb4df38f247803f65119578fc229
savas <savasusame@gmail.com>
2024-06-02 03:17:02 +0300

    chore: clear unnecessary fields

--- SimpleProjectTemplate.SharedLibrary/Constants/CustomClaims.cs
+++ SimpleProjectTemplate.SharedLibrary/Constants/CustomClaims.cs
@@ -4,6 +4,4 @@ public static class CustomClaims
 {
     public const string Role = "Role";
     public const string Permissions = "Permissions";
-    public const string LearnerId = "LearnerId";
-    public const string SubscriptionLevel = "SubscriptionLevel";
 }
